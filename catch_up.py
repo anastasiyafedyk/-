7b98e014508128cb703423b916759bf7adee8249
nastya_fedyk <anastasiyafedyk26@gmail.com>
2022-02-05 21:59:59 +0200

    first commit

--- catch_up.py
+++ catch_up.py
@@ -13,15 +13,15 @@ y2 = 300
 sprite1 = transform.scale(image.load("sprite1.png"), (100, 100))
 sprite2 = transform.scale(image.load("sprite2.png"), (100, 100))
 speed = 10
-
-game = True
 clock = time.Clock()
 FPS = 60
 
+game = True
 while game:
     window.blit(background,(0, 0))
     window.blit(sprite1, (x1, y1))
     window.blit(sprite2, (x2, y2))
+
 
     for e in event.get():
         if e.type == QUIT:
